99760fa52bcba739ab8320a12458ec7a755d0093
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-07-19 17:57:27 +0300

    Bump 125.6422.04

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StreamWebRTC",
-            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/114.5735.08/StreamWebRTC.zip",
-            checksum: "9b03bca9e35061d2d9d58aa7dd255c72b0fd0dc6ff4aa2ae1756b57cdbf99bf9"
+            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.04/StreamWebRTC.zip",
+            checksum: "d714c6d560d32ec68ada2c1e645b8e9e63e7de5d68a8c40e2151c3aab7af8a7d"
         ),
     ]
 )
