4c3130ba1468866272e5b35d126f51a4486d4bf1
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-11-26 11:02:35 +0000

    Update release script

--- Package.swift
+++ Package.swift
@@ -2,22 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "StreamWebRTC",
+    name: "WebRTC",
     platforms: [
       .iOS(.v13),
       .macOS(.v11)
     ],
     products: [
         .library(
-            name: "StreamWebRTC",
-            targets: ["StreamWebRTC"]
+            name: "WebRTC",
+            targets: ["WebRTC"]
         ),
     ],
     dependencies: [ ],
     targets: [
         .binaryTarget(
-            name: "StreamWebRTC",
-            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.06/StreamWebRTC.zip",
+            name: "WebRTC",
+            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.06/WebRTC.zip",
             checksum: "c3b690ec04727cf5510aeb073fc24156b74a074293a66d39be0a5c2570df1422"
         ),
     ]
---
+++ WebRTC.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |spec|
+  spec.name         = "WebRTC"
+  spec.version      = "125.6422.06"
+  spec.summary      = "Stream WebRTC"
+  spec.description  = spec.summary
+
+  spec.homepage = "https://getstream.io/video/"
+  spec.license = { type: "BSD-3", file: "LICENSE" }
+  spec.author = { "getstream.io" => "support@getstream.io" }
+  spec.social_media_url = "https://getstream.io"
+
+  spec.ios.deployment_target = '13.0'
+
+  spec.source = { http: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/#{spec.version}/WebRTC.xcframework.zip" }
+  spec.vendored_frameworks = "WebRTC.xcframework"
+end
