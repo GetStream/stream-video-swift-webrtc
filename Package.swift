770e1bcc726e6315c142b6f18c8bcaa30d340917
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-11-27 10:46:37 +0000

    Update xcframework path

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StreamWebRTC",
-            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.062/StreamWebRTC.zip",
+            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.062/StreamWebRTC.xcframework.zip",
             checksum: "44cacd9c591608c711203c8e934f2a4a04ee91d207eae4b6f7caae4b459069e7"
         ),
     ]
