0ff16a3a1c039cffdd956e7b12af3ccf2d272d57
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-04-14 14:56:41 +0100

    Delete vendor directory

--- vendor/bundle/ruby/3.1.0/gems/fastlane-plugin-stream_actions-0.3.76/lib/fastlane/plugin/stream_actions/version.rb
+++
@@ -1,5 +0,0 @@
-module Fastlane
-  module StreamActions
-    VERSION = '0.3.76'
-  end
-end
--- vendor/bundle/ruby/3.1.0/gems/rubocop-require_tools-0.1.2/lib/rubocop/require_tools.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'require_tools/version'
-require_relative 'cop/require/missing_require_statement'
