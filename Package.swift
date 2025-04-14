7615b5f95cbb117c00190f1aa893b0ca516f7924
Anka <runner@Mac-1744645088337.local>
2025-04-14 16:28:18 +0000

    Bump 125.6422.069

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StreamWebRTC",
-            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.068/StreamWebRTC.xcframework.zip",
-            checksum: "b214bdb65f2bd4a237cfb60528a91e5e7f19d11676b5339d8666d53f2ecd05a0"
+            url: "https://github.com/GetStream/stream-video-swift-webrtc/releases/download/125.6422.069/StreamWebRTC.xcframework.zip",
+            checksum: "0645e8aaeb60d4b35593b12d82e11b18f9131a7800b825aa363641fdc2b80ad3"
         ),
     ]
 )
