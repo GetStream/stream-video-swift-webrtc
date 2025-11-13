17061b47990ad8644061578da857f9686ad9955b
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-11-13 14:39:31 +0000

    Test workaround

--- fastlane/sync_gclient.sh
+++ fastlane/sync_gclient.sh
@@ -32,12 +32,3 @@ gclient config --spec 'solutions = [
 target_os = ["ios", "mac"]
 '
 gclient sync
-
-cd src
-./tools_webrtc/ios/build_ios_libs.py \
-  --deployment-target 13.0 \
-  --extra-gn-args \
-    is_debug=false \
-    use_goma=false \
-    use_rtti=true \
-    rtc_libvpx_build_vp9=true
